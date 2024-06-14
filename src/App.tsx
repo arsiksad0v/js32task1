3df4f2dcc6e81f08e3f3243b5372402d5fd45be4
Arstan <arsiksad0v.wgod@gmail.com>
2024-06-14 17:01:40 +0600

    Connected MovieTracker component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MovieTracker from './components/MovieTracker';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Movie Tracker</h1>
+      <MovieTracker />
     </div>
   );
-}
+};
 
 export default App;
